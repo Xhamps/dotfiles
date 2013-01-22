66d4b7c5a181fd6aca90ccb92a4f85560a40b518
Xhamps <xhamps@gmail.com>
2013-01-22 17:30:45 -0200

    first commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+cp files/aliases ~/.aliases
+cp files/bash_prompt ~/.bash_prompt
+cp files/functions ~/.functions
+cp files/exports ~/.exports
+cp files/bash_profile ~/.bash_profile
+cp files/gitconfig ~/.gitconfig
+cp files/gitignore ~/.gitignore
+
+mkdir -p ~/.bash/bin/
+
+curl http://www.paulhammond.org/2009/03/webkit2png-0.5/webkit2png-0.5.txt > ~/.bash/bin/webkit2png
+chmod +x ~/.bash/bin/webkit2png
+
+source ~/.bash_profile
